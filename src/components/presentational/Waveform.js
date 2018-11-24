5b61b2d4d073b0cb4cc18f9362225b1753a93fb8
yapjinai <jinai.jyap@gmail.com>
2018-11-24 18:06:59 -0500

    got waveform working, but now have to refactor everything

--- src/components/presentational/Waveform.js
+++ src/components/presentational/Waveform.js
@@ -1,12 +1,57 @@
 import React, { Component } from 'react';
+import Wavesurfer from 'react-wavesurfer';
 
 class Waveform extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playing: true,
+      pos: 0
+    };
+  }
+
   render() {
     return (
       <div className="Waveform">
-        Waveform
+        {this.renderWaveform()}
       </div>
     );
+  }
+
+  ////////////////////
+
+  handleTogglePlay = () => {
+    this.setState({
+      playing: !this.state.playing
+    });
+  }
+  handlePosChange = (e) => {
+    this.setState({
+      pos: e.originalArgs[0]
+    });
+  }
+
+  renderWaveform = () => {
+    if (this.props.currentSong) {
+      const waveformOptions = {
+        // interact: false,
+        height: 64,
+        fillParent: false,
+        scrollParent: true,
+        // minPxPerSec: 100
+      }
+
+      return (
+        <Wavesurfer
+          audioFile={this.props.currentSong.url}
+          pos={this.state.pos}
+          onPosChange={this.handlePosChange}
+          playing={this.state.playing}
+          options={waveformOptions}
+        />
+      )
+    }
   }
 }
 
