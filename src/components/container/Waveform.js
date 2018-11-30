92ccb69f71115a153fbc5dfe8e17aefd74b0f85a
yapjinai <jinai.jyap@gmail.com>
2018-11-30 15:19:44 -0500

    wavesurfer loops working

--- src/components/container/Waveform.js
+++ src/components/container/Waveform.js
@@ -7,75 +7,94 @@ import '../../css/Waveform.css';
 class Waveform extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      pos: 0
-    };
-  }
-
-  render() {
-    return (
-      <div className="Waveform">
-        {this.renderWaveform()}
-      </div>
-    );
-  }
-
-  ////////////////////
-
-  handlePosChange = (e) => {
-    this.setState({
-      pos: e.originalArgs[0]
-    });
-  }
-
-  handleFinish = () => {
-    console.log('finished');
-    // this.props.setPlaying(this.props.side, false)
-    this.props.playNextFromQueue()
-  }
-
-  renderWaveform = () => {
-    if (this.props.currentSong) {
-      const waveformOptions = {
+      pos: 0,
+      waveformOptions: {
         audioRate: this.props.audioRate,
         // backend: this.props.pitchShift ? 'WebAudio' : 'MediaElement',
 
         height: 128,
         fillParent: false,
         scrollParent: true,
-
-        interact: false,
-      }
-
-      const regionsObjects = {
+      },
+      regions: {
         loop: {
           id: 'loop',
-          start: 0,
-          end: 2,
+          start: 6,
+          end: 8,
           loop: true,
         }
       }
+    };
+  }
+
+  render() {
+    return (
+      <div className="Waveform">
+        {this.props.currentSong ? this.renderWaveform() : null}
+      </div>
+    );
+  }
+
+  ////////////////////
+
+  // WAVEFORM METHODS
+
+  handlePosChange = (e) => {
+    const newPos = e.originalArgs[0]
+    if (newPos < this.state.regions.loop.start || newPos > this.state.regions.loop.end) {
+      this.setState({
+        pos: newPos
+      });
+    }
+  }
+
+  handleFinish = () => {
+    console.log('finished');
+    this.props.playNextFromQueue()
+  }
+
+  // REGIONS METHODS
+
+  handleRegionUpdated = (e) => {
+    console.log('updating region');
+
+    const newRegions = {...this.state.regions}
+    const newLoop = {...this.state.loop}
+
+    newLoop.start = e.originalArgs[0].start
+    newLoop.end = e.originalArgs[0].end
+
+    newRegions.loop = newLoop
+    this.setState({
+      regions: newRegions
+    })
+  }
+
+  // RENDER
+
+  renderWaveform = () => {
 
       return (
         <Wavesurfer
           audioFile={this.props.currentSong.url}
           playing={this.props.playing}
           volume={this.props.volume}
-          options={waveformOptions}
 
+          options={this.state.waveformOptions}
           pos={this.state.pos}
+
           onPosChange={this.handlePosChange}
           onFinish={this.handleFinish}
         >
 
           <Regions
-            regions={regionsObjects}
+            regions={this.state.regions}
+            onRegionUpdated={this.handleRegionUpdated}
           />
 
         </Wavesurfer>
       )
-    }
   }
 }
 
--- src/reducers/channels.js
+++ src/reducers/channels.js
@@ -1,5 +1,12 @@
 const defaultChannel = {
-  currentSong: null,
+  // currentSong: null,
+  currentSong: {
+    id: 116,
+    title: "Dead Island",
+    artist: "Kitty",
+    bpm: 117,
+    url: "http://localhost:3001/DeadIsland.mp3",
+  },
   playing: false,
 
   volume: .5,
