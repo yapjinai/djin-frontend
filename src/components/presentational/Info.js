7dc0ea0fded4e2d36412274fa503ad31eaf6d74c
yapjinai <jinai.jyap@gmail.com>
2018-12-06 11:27:03 -0500

    styled and added to info page

--- src/components/presentational/Info.js
+++ src/components/presentational/Info.js
@@ -3,59 +3,78 @@ import React from 'react';
 const Info = () => {
   return (
     <div className='Info hidden'>
-      <h3>Shortcuts:</h3>
+      <h3>Keyboard shortcuts:</h3>
       <ul>
-        <li>
-          Space: play/pause master
-        </li>
-        <li>
-          ←, →: crossfade left/right<br />
-          (with Shift: hard crossfade)
-        </li>
-        <li>
-          ↑, ↓: BPM up/down<br />
-          (with Shift: faster up/down)
-        </li>
 
-        <br />
+        <span>
+          <label>Master controls</label>
+          <br />
 
-        <li>
-          Q, P: play/pause left/right
-        </li>
-        <li>
-          S, L: toggle looping left/right
-        </li>
+          <li>
+            <strong>Space</strong>: play/pause master
+          </li>
+          <li>
+            <strong>←</strong>, <strong>→</strong>: crossfade left/right<br />
+            (with <strong>Shift</strong>: hard crossfade)
+          </li>
+          <li>
+            <strong>↑</strong>, <strong>↓</strong>: BPM up/down<br />
+            (with <strong>Shift</strong>: faster up/down)
+          </li>
 
-        <br />
-        <label>Nudge loops</label>
-        <br />
-        (with Shift: nudge faster)
+          <br />
+
+          <li>
+            <strong>Q</strong>, <strong>P</strong>: play/pause left/right
+          </li>
+          <li>
+            <strong>S</strong>, <strong>L</strong>: toggle looping left/right
+          </li>
+          
+          <br />
+          <label>Seek forwards/backwards</label>
+          <br />
+          (with <strong>Shift</strong>: seek faster)
 
 
-        <li>
-          E, U: nudge loop start backwards left/right
-        </li>
-        <li>
-          R, I: nudge loop start forwards left/right
-        </li>
+          <li>
+            <strong>1</strong>, <strong>9</strong>: seek backwards left/right
+          </li>
+          <li>
+            <strong>2</strong>, <strong>0</strong>: seek forwards left/right
+          </li>
+        </span>
 
-        <br />
+        <span>
+          <label>Loop controls</label>
+          <br />
+          (with <strong>Shift</strong>: nudge faster)
 
-        <li>
-          C, N: nudge loop end backwards left/right
-        </li>
-        <li>
-          V, M: nudge loop end forwards left/right
-        </li>
+          <li>
+            <strong>E</strong>, <strong>U</strong>: nudge loop start backwards left/right
+          </li>
+          <li>
+            <strong>R</strong>, <strong>I</strong>: nudge loop start forwards left/right
+          </li>
 
-        <br />
+          <br />
 
-        <li>
-          D, J: halve loop left/right
-        </li>
-        <li>
-          F, K: double loop left/right
-        </li>
+          <li>
+            <strong>C</strong>, <strong>N</strong>: nudge loop end backwards left/right
+          </li>
+          <li>
+            <strong>V</strong>, <strong>M</strong>: nudge loop end forwards left/right
+          </li>
+
+          <br />
+
+          <li>
+            <strong>D</strong>, <strong>J</strong>: halve loop left/right
+          </li>
+          <li>
+            <strong>F</strong>, <strong>K</strong>: double loop left/right
+          </li>
+        </span>
 
 
       </ul>
