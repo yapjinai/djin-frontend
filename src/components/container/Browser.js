993e5353acbe2a3aa0fc7781d82fb48bd638fcac
yapjinai <jinai.jyap@gmail.com>
2018-12-09 23:02:17 -0500

    fix api url

--- src/components/container/Browser.js
+++ src/components/container/Browser.js
@@ -16,8 +16,8 @@ import '../../css/Browser.css';
 import BrowserSong from '../presentational/BrowserSong';
 
 const uuid = require('uuid/v4');
-const apiUrl = 'http://localhost:3000'
-// const apiUrl = 'http://djin-api.herokuapp.com'
+// const apiUrl = 'http://localhost:3000'
+const apiUrl = 'http://djin-api.herokuapp.com'
 
 class Browser extends Component {
   constructor() {
